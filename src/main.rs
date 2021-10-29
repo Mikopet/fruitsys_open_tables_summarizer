d1574bfb818296c82e01da725710b0105c244cda
Peter Mikola <mikopet@gmail.com>
2021-10-29 10:14:24 +0200

    add login to fruitsys

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::env;
+
+const LOGIN_URL: &str = "https://cloud.fruitsys.hu/raktar/index.php";
+const REQUEST_URL: &str = "https://cloud.fruitsys.hu/raktar/nyitott_asztalok.php";
+
+fn main() -> Result<(), reqwest::Error> {
+    dotenv().ok();
+
+    let username = env::var("FRUITSYS_USERNAME").expect("Failed to read FRUITSYS_USERNAME");
+    let password = env::var("FRUITSYS_PASSWORD").expect("Failed to read FRUITSYS_PASSWORD");
+
+    let client = reqwest::blocking::Client::builder()
+        .cookie_store(true)
+        .build()?;
+
+    let _login = client
+        .post(LOGIN_URL)
+        .form(&[
+            ("user_name", username),
+            ("user_password", password),
+            ("login", "Bejelentkezés".into()),
+        ])
+        .send()?;
+
+    let body = client.get(REQUEST_URL).send()?.text();
+    println!("body = {:?}", body);
+
+    Ok(())
 }
